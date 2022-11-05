3a42ffd4c6430a09bbdc4421fef4119c1b3bdf39
Rebeckaspolander <rebeckaspolander@yahoo.se>
2022-11-05 22:16:50 +0100

    Server.js code and gitignore.

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import * as path from "path";
+import bodyParser from "body-parser";
+
+const app = express();
+
+const MOVIES = [
+    {
+        title: "The Matrix from server",
+        plot: "A computer hacker learns from mysterious rebels about the true nature of his reality and his rol in the war against its controllers.",
+        year: 999
+    },
+    {
+        title: "The Color Purple",
+        plot: "A black Southern woman (Whoopi Goldberg) struggles to find her identity after suffering years of abuse from her father and others over 40 years.",
+        year: 1985
+    }
+];
+
+app.use(bodyParser.json());
+
+app.get("/api/movies", (req, res) => {
+    res.json(MOVIES)
+});
+
+app.post("/api/movies", (req, res) => {
+    const {title, year, plot} = req.body;
+    MOVIES.push({title, year, plot});
+    res.sendStatus(200);
+});
+
+app.use(express.static(path.resolve("..", "client" ,"dist")));
+app.use((req, res) => {
+    res.sendFile(path.resolve("..", "client", "dist", "index.html"))
+});
+
+const server = app.listen(3000, () =>{
+    console.log("listening on http://localhost:" +  server.address().port);
+});
